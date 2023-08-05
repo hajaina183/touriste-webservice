0cd481d07e15a753612985c9995dcc50e0c55800
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2023-08-06 00:58:55 +0400

    web service get commentaire

--- controllers/parcController.js
+++ controllers/parcController.js
@@ -12,6 +12,34 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/listeCommentaire', (req, res) => {
+    var parc = new Parc({
+        nom: req.body.nom
+    });
+    Parc.find({ nom: req.body.nom }, function (err, docs) {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ message: 'Une erreur est survenue lors de la récupération des commentaires.' });
+        }
+
+        if (!docs || docs.length === 0) {
+            console.log('Aucun parc trouvé avec ce nom.' )
+            return res.status(404).json({ message: 'Aucun parc trouvé avec ce nom.' });
+        }
+
+        const parcTrouve = docs[0];
+
+        if (!parcTrouve.commentaire || parcTrouve.commentaire.length === 0) {
+            console.log('Aucun commentaire trouvé pour ce parc. taille 0')
+            return res.status(404).json({ message: 'Aucun commentaire trouvé pour ce parc.' });
+        }
+
+        res.json(parcTrouve.commentaire);
+    });
+});
+
+  
+
 router.get('/:id', (req, res) => {
     if (!ObjectId.isValid(req.params.id))
         return res.status(400).send(`No record with given id : ${req.params.id}`);
